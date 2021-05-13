8cb86bf25e3cece7bc58cbd06dc5d39d23aa8fba
Moises Perez <perezmoises132@gmail.com>
2021-05-13 16:20:57 -0700

    Fixed load.js

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -1,4 +1,4 @@
-class Player extends Phaser.GameOvjects.Sprite {
+class Player extends Phaser.GameObjects.Sprite {
     constructor (scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
         scene.add.existing(this);
