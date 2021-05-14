d36734fb8568257ffb2771ffd2b76d8632f5e007
Moises Perez <perezmoises132@gmail.com>
2021-05-14 09:55:32 -0700

    Changes to load.

--- src/scenes/load.js
+++ src/scenes/load.js
@@ -4,7 +4,6 @@ class Load extends Phaser.Scene {
     }
     preload() {
         //Loading assets
-        this.load.image('enemy1', './assets/spaceships_008.png');
         this.load.image('playerShip1', './assets/playerShip1_green.png');
         this.load.image('Background', './assets/tempbackground.png');
         this.load.image('trail', './assets/trail.png');
